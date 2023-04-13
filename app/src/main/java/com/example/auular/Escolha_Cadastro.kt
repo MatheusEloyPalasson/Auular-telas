cc699a3bed594d628a8bcedd3f771a45c5ee9eb3
MatheusEloyPalasson <retheus.palasson@gmail.com>
2023-04-13 00:01:42 -0300

    Initial commit

---
+++ app/src/main/java/com/example/auular/Escolha_Cadastro.kt
@@ -0,0 +1,11 @@
+package com.example.auular
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Escolha_Cadastro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_escolha_cadastro)
+    }
+}
