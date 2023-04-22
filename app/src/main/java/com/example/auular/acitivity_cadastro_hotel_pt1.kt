5c35bbb5e59339f173e7312ff840c0bdc44d3f45
WallisonGalvao <wallison08@gmail.com>
2023-04-22 00:59:46 -0300

    fix: add keys to strings.xml, modified the inputs of activity_cadastro and created new activity

---
+++ app/src/main/java/com/example/auular/acitivity_cadastro_hotel_pt1.kt
@@ -0,0 +1,11 @@
+package com.example.auular
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class acitivity_cadastro_hotel_pt1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_acitivity_cadastro_hotel_pt1)
+    }
+}
