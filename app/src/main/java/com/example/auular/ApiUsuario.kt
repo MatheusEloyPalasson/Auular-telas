caf4ad5a352038626c5da457db2e3db0afbfdac4
WallisonGalvao <wallison08@gmail.com>
2023-04-23 14:57:26 -0300

    feat: add data class PetTutor

--- app/src/main/java/com/example/auular/ApiUsuario.kt
+++ app/src/main/java/com/example/auular/ApiUsuario.kt
@@ -8,6 +8,7 @@ import retrofit2.http.Query;
 
 interface ApiUsuario {
     @GET("usuarios")
-fun getLogin(@Query("login_senha") loginSenha: String) : Call<List<PetTutor>>
+    fun getLogin(@Query("login_senha") loginSenha: String) : Call<List<PetTutor>>
+
 
 }
---
+++ app/src/main/java/com/example/auular/PetTutor.kt
@@ -0,0 +1,16 @@
+package com.example.auular
+
+import com.google.gson.annotations.SerializedName
+
+
+data class PetTutor(
+    val petTutorId:Integer,
+    var name:String,
+    @field:SerializedName("email") var email:String,
+    @field:SerializedName("password") var password: String,
+    val documentId:String,
+    var phoneNumber:String,
+    var isAuthenticated:Boolean,
+    var active:Boolean
+
+)
