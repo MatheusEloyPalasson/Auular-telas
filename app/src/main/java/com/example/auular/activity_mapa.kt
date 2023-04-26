d391b1cd5c9e12cb93ee5f72185209538abfa870
MatheusEloyPalasson <retheus.palasson@gmail.com>
2023-04-26 20:05:01 -0300

    Add map settings

--- app/src/main/java/com/example/auular/activity_mapa.kt
+++ app/src/main/java/com/example/auular/activity_mapa.kt
@@ -1,37 +1,118 @@
 package com.example.auular
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
+import com.mapbox.geojson.Point
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
-
-var mapView: MapView? = null
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class activity_mapa : AppCompatActivity() {
+    private var mapView: MapView? = null
+    var longitude = 00.00
+    var latitude = 00.00
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView = findViewById(R.id.mapView)
-        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
-    }
-    override fun onStart() {
-        super.onStart()
-        mapView?.onStart()
+        mapView?.getMapboxMap()?.loadStyleUri(
+            Style.MAPBOX_STREETS,
+            object : Style.OnStyleLoaded {
+                override fun onStyleLoaded(style: Style) {
+                    addAnnotationToMap()
+                }
+            }
+        )
+        val buttonPesquisar = findViewById<Button>(R.id.bt_pesquisar)
+
+        var cep = findViewById<EditText>(R.id.et_cep)
+
+        val ApiUsuario = BrasilApi.findPlace()
+
+        buttonPesquisar.setOnClickListener {
+
+            val result: Call<Address> = ApiUsuario.buscar(cep.text.toString().toLong())
+
+            result.enqueue(object : Callback<Address> {
+                override fun onResponse(call: Call<Address>, response: Response<Address>) {
+                    if (response.isSuccessful) {
+                        val address = response.body()
+                        longitude = address!!.location.coordinates.longitude
+                        latitude = address.location.coordinates.latitude
+                    }
+                }
+
+                override fun onFailure(call: Call<Address>, t: Throwable) {
+                    Toast.makeText(
+                        baseContext, "Erro na API: ${t.message}",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    t.printStackTrace()
+                }
+            })
+        }
     }
 
-    override fun onStop() {
-        super.onStop()
-        mapView?.onStop()
+
+    private fun addAnnotationToMap() {
+// Create an instance of the Annotation API and get the PointAnnotationManager.
+        bitmapFromDrawableRes(
+            this@activity_mapa,
+            R.drawable.red_marker
+        )?.let {
+            val annotationApi = mapView?.annotations
+            val pointAnnotationManager = annotationApi?.createPointAnnotationManager(mapView!!)
+// Set options for the resulting symbol layer.
+            val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
+// Define a geographic coordinate.
+                .withPoint(Point.fromLngLat(longitude, latitude))
+// Specify the bitmap you assigned to the point annotation
+// The bitmap will be added to map style automatically.
+                .withIconImage(it)
+// Add the resulting pointAnnotation to the map.
+            pointAnnotationManager?.create(pointAnnotationOptions)
+        }
     }
 
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView?.onLowMemory()
-    }
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
 
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView?.onDestroy()
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) {
+            return null
+        }
+        return if (sourceDrawable is BitmapDrawable) {
+            sourceDrawable.bitmap
+        } else {
+// copying drawable object to not manipulate on the same reference
+            val constantState = sourceDrawable.constantState ?: return null
+            val drawable = constantState.newDrawable().mutate()
+            val bitmap: Bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        }
     }
 }
