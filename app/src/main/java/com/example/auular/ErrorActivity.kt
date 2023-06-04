69f84d3ec2816e569b6c1c11576f87b307096e2a
Wallison Galvao <wallison08@gmail.com>
2023-06-04 17:54:50 -0300

    feat: add setOnClickListener to HotelAdapter

--- app/src/main/java/com/example/auular/ErrorActivity.kt
+++ app/src/main/java/com/example/auular/ErrorActivity.kt
@@ -14,11 +14,11 @@ class ErrorActivity : AppCompatActivity() {
         val container = findViewById<LinearLayout>(R.id.container)
         val container2 = findViewById<LinearLayout>(R.id.container2)
 
-        // Crie uma instância do fragmento
+
         val appName = AppName()
         val buttonHome = BotaoHome()
 
-// Use o supportFragmentManager para iniciar a transação e adicionar o fragmento ao contêiner
+// Usando o supportFragmentManager para iniciar a transação e adicionar o fragmento ao contêiner
         supportFragmentManager.beginTransaction()
             .add(R.id.container, appName)
             .commit()
