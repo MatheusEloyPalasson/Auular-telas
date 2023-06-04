336c44b2cecb0b5a2bc824054a2241095ce2c656
gabrielsouzaexe <gabrielsouza628@gmail.com>
2023-06-04 19:26:16 -0300

    fix: remove useless

--- app/src/main/java/com/example/auular/api/ApiUrl.kt
+++ app/src/main/java/com/example/auular/api/ApiUrl.kt
@@ -4,9 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiUrl {
-    var BASE_URL = "http://192.168.0.21:8080"
 
-//    var BASE_URL = "http://54.162.20.136:8080"
+    var BASE_URL = "http://54.162.20.136:8080"
 //    var BASE_URL = "http://172.26.16.1:8080"
 
     fun getApiUsuarios() : ApiUsuario {
